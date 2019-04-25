078a8a1d002fb6952e99e3e11050c6f649cebdde
Irvin Lin <irvinln@gmail.com>
2019-04-25 18:19:22 +0800

    set sample location coords

--- App.js
+++ App.js
@@ -3,23 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { MapView, Permissions } from 'expo';
 
+const locations = require('./locations.json');
+
 export default class App extends React.Component {
   state = {
     latitude: null,
-    longitude: null
-  }
+    longitude: null,
+    locations: locations
+  };
 
   async componentDidMount () {
-    const { status } = await Permissions.getAsync(Permissions.LOCATION)
+    const { status } = await Permissions.getAsync(Permissions.LOCATION);
 
     if (status !== 'granted') {
-      const response = await Permissions.getAsync(Permissions.LOCATION)
+      const response = await Permissions.getAsync(Permissions.LOCATION);
     }
+
     // 2 params
     navigator.geolocation.getCurrentPosition(
       ({ coords: {latitude, longitude} }) => this.setState({longitude, latitude}, () => console.log('hi') ),
       (error) => console.log('error')
     )
+    
+    // how does the inner array destructure?
+    // https://stackoverflow.com/questions/37606427/nested-es6-array-destructuring
+    // To get the 1st child-array's 1st element
+    // let [[x]] = [[1], [1, 2], [1, 2, 3]]
+    // To get the 2nd child-array's 1st element
+    // let [ , [x]] = [[1], [1, 2], [1, 2, 3]]
+
+    const { locations: [ , sampleLocation ] } = this.state
+
+    this.setState({
+      desLatitude: sampleLocation.coords.latitude,
+      desLongitude: sampleLocation.coords.longitude
+    }, () => {});
+
   }
 
   render() {
